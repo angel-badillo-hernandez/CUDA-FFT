20d32ac07600d67273dc79e8e5121aee7084d95d
Angel Badillo <badilloa022402@gmail.com>
2022-10-29 16:03:28 -0500

    completed code

--- AngelBadilloA4.cu
+++ AngelBadilloA4.cu
@@ -1,31 +1,43 @@
 //**************************************************************
 // Assignment #4
-// Name: Angel Badillo, and James
-// GPU Programming Date: Date of Submission (M/D/Y)
+// Name: Angel Badillo, and James Nealley
+// GPU Programming Date: 11/03/22
 //***************************************************************
-// Place your general program documentation here. It should
-// be quite a few lines explaining the programs duty carefully.
-// It should also indicate how to run the program and data
-// input format, filenames etc
+// How to run:
+// This program is to be run on the TACC cluster using the SBATCH
+// shell script named "AngelBadilloA4Script".
+// The command to be run in the bash terminal is:
+// sbatch AngelBadilloA4Script
+//
+// Description:
+// This program, written in CUDA C, aims to implement a Fast
+// Fourier Transform (FTT) algorithm, specifically the
+// Cooley-Turkey FFT algorithm (Radix-2). The program peforms the
+// FFT for N Fourier coefficients (8192) from X(0) to X(N-1),
+// given N samples (8192). The first 8 samples are hardcoded,
+// to array R[N] and I[N], but the rest are initialized to 0.
+// Once the full calculation for N Fourier coefficients are
+// completed after invoking the kernel, the program will
+// print the first 8 outputs for X(0) to X(7).
 //*****************************************************************
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
-
-// No. of samples
+// No. of samples and Fourier coefficients
 const int N_SAMPLES = 8192;
 
 // Constant for pi
 const double PI = 3.1415926535897932;
 
-// Byte size of type double
+// Byte size of array of doubles with 8192 elements;
 const int NBYTE_SIZE = N_SAMPLES * sizeof(double);
 
 /**
  * @brief Represents a Complex number.
- * Contains real and imaginary part.
- * 
+ * Contains real part (double CmplxNum::a) and imaginary part
+ * (double CmplxNum::bi).
+ *
  */
 struct CmplxNum
 {
@@ -35,11 +47,12 @@ struct CmplxNum
 
 /**
  * CmplxAdd
- * @brief 
- * Calculates the sum of CmplxNum X and Y, then returns sum as CmplxNum.
- * @param X Augend, a CmplxNum
- * @param Y Addend, a CmplxNum
- * @return Sum as struct CmplxNum 
+ * @brief
+ * Calculates the addition of complex numbers (CmplxNum) X and Y, then returns
+ * sum as CmplxNum.
+ * @param X The augend, a CmplxNum
+ * @param Y The addend, a CmplxNum
+ * @return The sum as struct CmplxNum
  */
 __device__ struct CmplxNum CmplxAdd(struct CmplxNum X, struct CmplxNum Y)
 {
@@ -50,83 +63,52 @@ __device__ struct CmplxNum CmplxAdd(struct CmplxNum X, struct CmplxNum Y)
 /**
  * CmplxMult
  * @brief
- * Multiplies two CmplxNum together, then returns product as a CmplxNum.
- * @param X Multiplicand, as a CmplxNum
- * @param Y Multiplier, as a CmplxNum
- * @return struct CmplxNum 
+ * Calculates the multiplication of complex numbers (CmplxNum) X and Y, then returns
+ * product as CmplxNum.
+ * @param X The multiplicand, a CmplxNum
+ * @param Y The multiplier, a CmplxNum
+ * @return The product as struct CmplxNum
  */
 __device__ struct CmplxNum CmplxMult(struct CmplxNum X, struct CmplxNum Y)
-{   
+{
     // X = a + bi
     // Y = c + di
     // XY = (a+bi)(c+di) = (ac−bd) + (ad+bc)i
-    struct CmplxNum Z = {.a = (X.a * Y.a) - (X.bi*Y.bi), .bi = (X.a * Y.bi) + (X.bi*Y.a)};
+    struct CmplxNum Z = {.a = (X.a * Y.a) - (X.bi * Y.bi), .bi = (X.a * Y.bi) + (X.bi * Y.a)};
     return Z;
 }
 
-__device__ struct CmplxNum evenPartAtm(double R[], double I[], int m, int N, int k)
-{
-    struct CmplxNum funcX2m = {.a = R[2 * m], .bi = I[2 * m]};
-    struct CmplxNum eulerPart = {.a = cos(2 * PI * 2 * m * k / N), .bi = -sin(2 * PI * 2 * m * k / N)};
-    return CmplxMult(funcX2m, eulerPart);
-}
-
-__device__ void evenPartOfK(double XR[], double XI[], double R[], double I[], int N, int k)
-{
-    for (int n = 0; n <= (N / 2) - 1; n++)
-    {
-        struct CmplxNum result = evenPartAtm(R, I, n, N, k);
-        printf("Even n=%d: %f + %fi\n", n, result.a, result.bi);
-        XR[k] += result.a;
-        XI[k] += result.bi;
-    }
-}
-
-__device__ struct CmplxNum oddPartAtm(double R[], double I[], int m, int N, int k)
-{
-    struct CmplxNum funcX2mP1 = {.a = R[(2 * m) + 1], .bi = I[(2 * m) + 1]};
-    struct CmplxNum eulerPart = {.a = cos(2 * PI * 2 * m * k / N), .bi = -sin(2 * PI * 2 * m * k / N)};
-    return CmplxMult(funcX2mP1, eulerPart);
-}
-
-__device__ struct CmplxNum twiddleFactor(int N, int k)
-{
-    struct CmplxNum tFactor = {.a = cos(2 * PI * k / N), .bi = -sin(2 * PI * k / N)};
-    return tFactor;
-}
-
-__device__ void oddPartOfK(double XR[], double XI[], double R[], double I[], int N, int k)
-{
-    for (int n = 0; n <= (N / 2) - 1; n++)
-    {
-        struct CmplxNum result = CmplxMult(twiddleFactor(N, k),oddPartAtm(R, I, n, N, k));
-        printf("Odd n=%d: %f + %fi\n", n, result.a, result.bi);
-        XR[k] += result.a;
-        XI[k] += result.bi;
-    }
-}
-
-
 /**
  * computeFFT
- * @brief Compute K no. of FFT coefficients with N no. of samples.
- *        Implements the Cooley-Turkey FFT algorithm (AKA Radix-2).
- * 
- * @param R real part of samples for x(n)
- * @param I imaginary part of samples for x(n)
- * @param XR real part of FTT coefficients
- * @param XI imaginary part of FFT coefficients
+ * @brief 
+ * Implements the Cooley-Turkey FFT algorithm (AKA Radix-2).
+ * Computes the output for a total of N no. FFT coefficients with N no. of 
+ * samples."Returns" (or modifies via reference) array of doubles XR and XI to 
+ * have the output for N no. of FFT coefficients X(0) to X(N-1).
+ * @param XR an array of doubles of size 8192 containing real parts of output 
+ *           for N no. of FTT coefficients.
+ * @param XI an array of doubles of size 8192 containing imaginary parts of output 
+ *          for N no. of FFT coefficients.
+ * @param R an array of doubles of size 8192 containing real part of samples for 
+ *           the function x(n).
+ * @param I an array of doubles of size 8192 containing imaginary part of samples 
+ *          for the function x(n)
  */
-__global__ void computeFFT(double* XR, double* XI, double* R, double* I)
+__global__ void computeFFT(double *XR, double *XI, double *R, double *I)
 {
     // Global index
     int k = threadIdx.x + blockIdx.x * blockDim.x;
 
+    // Calculate twiddle factor
     struct CmplxNum tFactor = {.a = cos(2 * PI * k / N_SAMPLES), .bi = -sin(2 * PI * k / N_SAMPLES)};
+
+    // Initialize variables for calculation of even and odd parts for calculation of X(k) and X(k + N/2)
     struct CmplxNum evenPart = {.a = 0, .bi = 0};
     struct CmplxNum oddPart = {.a = 0, .bi = 0};
-    for(int m = 0; m <= N_SAMPLES/2-1; m++)
-    {   
+
+    // Calculate the sums of the even and odd parts for calculation of X(k) and X(k + N/2)
+    for (int m = 0; m <= N_SAMPLES / 2 - 1; m++)
+    {
         // E(k)
         struct CmplxNum funcX2m = {.a = R[2 * m], .bi = I[2 * m]};
         struct CmplxNum eulerPart = {.a = cos(2 * PI * 2 * m * k / N_SAMPLES), .bi = -sin(2 * PI * 2 * m * k / N_SAMPLES)};
@@ -135,21 +117,28 @@ __global__ void computeFFT(double* XR, double* XI, double* R, double* I)
 
         // O(k)
         struct CmplxNum funcX2mP1 = {.a = R[(2 * m) + 1], .bi = I[(2 * m) + 1]};
-        struct CmplxNum resOdd = CmplxMult(funcX2mP1,eulerPart);
+        struct CmplxNum resOdd = CmplxMult(funcX2mP1, eulerPart);
         oddPart = CmplxAdd(oddPart, resOdd);
     }
-    // Adding even parts
-    XR[k] += evenPart.a;
-    XI[k] += evenPart.bi;
-    XR[k+N_SAMPLES/2] += evenPart.a;
-    XI[k+N_SAMPLES/2] += evenPart.bi;
 
-    // Adding odd parts
+    // Adding even part for E(k)
+    XR[k] = evenPart.a;
+    XI[k] = evenPart.bi;
+
+    // Adding even part for E(k + N/2)
+    XR[k + N_SAMPLES / 2] = evenPart.a;
+    XI[k + N_SAMPLES / 2] = evenPart.bi;
+
+    // Adjusting odd parts by twiddle factor
     struct CmplxNum temp = CmplxMult(tFactor, oddPart);
+
+    // Adding odd part for O(k)
     XR[k] += temp.a;
     XI[k] += temp.bi;
-    XR[k+N_SAMPLES/2] -= temp.a;
-    XI[k+N_SAMPLES/2] -= temp.bi;
+
+    // Subtracting odd part for O(k + N/2)
+    XR[k + N_SAMPLES / 2] -= temp.a;
+    XI[k + N_SAMPLES / 2] -= temp.bi;
 }
 
 int main()
@@ -157,15 +146,15 @@ int main()
     // Real and imaginary components of samples
     double R[N_SAMPLES] = {3.6, 2.9, 5.6, 4.8, 3.3, 5.9, 5.0, 4.3};
     double I[N_SAMPLES] = {2.6, 6.3, 4.0, 9.1, 0.4, 4.8, 2.6, 4.1};
-    double* R_d;
-    double* I_d;
+    double *R_d;
+    double *I_d;
 
     // Output for Fourier coefficients
     double XR[N_SAMPLES];
     double XI[N_SAMPLES];
-    double* XR_d;
-    double* XI_d;
-    
+    double *XR_d;
+    double *XI_d;
+
     // Allocate memory for R_d, I_d, XR_d, and XI_d
     cudaMalloc((void **)&R_d, NBYTE_SIZE);
     cudaMalloc((void **)&I_d, NBYTE_SIZE);
@@ -178,14 +167,13 @@ int main()
     cudaMemcpy(XR_d, XR, NBYTE_SIZE, cudaMemcpyHostToDevice);
     cudaMemcpy(XI_d, XI, NBYTE_SIZE, cudaMemcpyHostToDevice);
 
-
     // Dimensions of grid & block
-    dim3 dimGrid(4,1);
-    dim3 dimBlock(1024,1);
+    dim3 dimGrid(4, 1);
+    dim3 dimBlock(1024, 1);
 
     // Call kernel to compute FFT
     computeFFT<<<dimGrid, dimBlock>>>(XR_d, XI_d, R_d, I_d);
-    
+
     // Copy XR_d & XI_d to XR & XI
     cudaMemcpy(XR, XR_d, NBYTE_SIZE, cudaMemcpyDeviceToHost);
     cudaMemcpy(XI, XI_d, NBYTE_SIZE, cudaMemcpyDeviceToHost);
@@ -196,27 +184,15 @@ int main()
     cudaFree(XR_d);
     cudaFree(XI_d);
 
-    // Print the N Fourier Coefficients from X(0) to X(7)
+    // Print output for the first 8 Fourier Coefficients from X(0) to X(7)
     printf("==========================================================================\n");
     printf("TOTAL PROCESSED SAMPLES: %d\n", N_SAMPLES);
     printf("==========================================================================\n");
     for (int i = 0; i < 8; ++i)
     {
-        printf("XR[%d]: %f          XI[%d]: %fi\n", i, XR[i], i, XI[i]);
+        printf("XR[%d]: %.6f          XI[%d]: %.6fi\n", i, XR[i], i, XI[i]);
         printf("==========================================================================\n");
     }
-    
+
     return EXIT_SUCCESS;
 }
-
-// =====================================
-// TOTAL PROCESSED SAMPLES :8192
-// =====================================
-// 35.400002 + 33.900002j  [K= 0]
-// 35.485325 + 33.799137j  [K= 1]
-// 35.570263 + 33.697975j  [K= 2]
-// 35.654804 + 33.596500j  [K= 3]
-// 35.738964 + 33.494720j  [K= 4]
-// 35.822723 + 33.392647j  [K= 5]
-// 35.906090 + 33.290264j  [K= 6]
-// 35.989056 + 33.187588j  [K= 7]
